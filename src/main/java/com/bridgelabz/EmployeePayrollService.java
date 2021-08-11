1e1d69be482635869298ff2349910a84698931bd
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-11 21:34:35 +0530

    Added: Welcome_employee

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,43 @@
+package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * this is main class EmployeePayrollService
+ */
+public class EmployeePayrollService {
+    public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO}
+    // Declaration of list employeePayrollList
+    private List<EmployeePayrollData> employeePayrollList;
+
+    // this is constructor to initialize employeePayrollList List.
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    // this is method to read employee data form console by using Scanner class
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.print("Welcome to Employee PayRollService\n");
+        System.out.print("Enter Employee ID:\n ");
+        int id = consoleInputReader.nextInt();
+        System.out.print("Enter the Employee Name:\n ");
+        String name = consoleInputReader.next();
+        System.out.print("Enter the Employee Salary:\n ");
+        Double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    // this is method to write employee data into console.
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+    }
+}
---
+++ src/test/java/com/bridgelabz/AppTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * welcome to Employee pay roll
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    private void assertTrue(boolean b) {
+    }
+}
